75c51a806f71f8c0bb30d91862bacde5dc197082
zqzal <936952020@qq.com>
2019-08-14 14:58:28 +0800

    Notification Use

--- app/src/androidTest/java/com/xuxi/notificationtest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/xuxi/notificationtest/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.xuxi.notificationtest;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- app/src/main/java/com/xuxi/notificationtest/MainActivity.java
+++ app/src/main/java/com/xuxi/notificationtest/MainActivity.java
@@ -1,13 +1,45 @@
 package com.xuxi.notificationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.core.app.NotificationCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendNotice = findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.send_notice:
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = new NotificationCompat.Builder(this,"default")
+                        .setContentTitle("This is content title")
+                        .setContentText("This is content text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                        .build();
+                manager.notify(1,notification);
+
+                break;
+            default:
+                break;
+        }
+    }
+
 }
